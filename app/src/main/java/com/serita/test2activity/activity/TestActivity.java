8222909a5264d5d2d79cc6eef71a55159c5eba93
xiaochuan <1298585961@qq.com>
2017-01-06 19:30:26 +0800

    修改了TestActivity

--- app/src/main/java/com/serita/test2activity/activity/TestActivity.java
+++ app/src/main/java/com/serita/test2activity/activity/TestActivity.java
@@ -1,22 +1,34 @@
 package com.serita.test2activity.activity;
 
+import android.widget.TextView;
+
+import com.serita.test2activity.R;
+
 /**
  * Created by 12985 on 2017/1/6.
  */
 
 public class TestActivity extends BaseActivity {
+    private TextView tv;
     @Override
     protected int getLayoutId() {
-        return 0;
+        return R.layout.activity_main;
     }
 
     @Override
     protected void initView() {
-
+        tv = (TextView) findViewById(R.id.tv);
+        tv.setOnClickListener(v ->onClick());
     }
 
     @Override
     protected void initData() {
+
+    }
+
+    private void onClick(){
+//        tv.setText("结果:"+Jni.patch("1","2","3"));
+        tv.setText("结果:");
 
     }
 }
